172e49b38460a08680b772a0932b47d48a1bb82c
vinay7299 <vinaytiwari7299@gmail.com>
2023-06-05 20:01:47 +0530

    bdfxbdf

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -3,7 +3,7 @@ import ListRender from "./ListRender";
 
 const Next7Days = (props) => {
 
-  const today = new Date("9/28/2022");
+  const today = new Date("9/27/2022");
 
   const withinWeek = (date) => {
     const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
